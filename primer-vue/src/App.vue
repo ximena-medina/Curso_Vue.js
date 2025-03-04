6631d94de350b74024a4ba304983c9ff47efe160
ximena <xime.mediin@gmail.com>
2025-03-04 10:16:31 -0600

    New feature :sparkles: Agregar key al v-for

--- primer-vue/src/App.vue
+++ primer-vue/src/App.vue
@@ -1,19 +1,28 @@
 <script setup>
-// const name = "vue dinamico" 
-// const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
-// </script>
-
-// <template>
-//   <h1>Hola vue.js {{ name.toUpperCase() }}</h1>
-//   <ul>
-//     <li v-for="(fruta, index) in arrayFrutas" :key="index"> 
-//       {{ index }} - {{ fruta }}
-//     </li>
-//   </ul>
-// </template>
-
-// <style>
-// h1 {
-//   color: blue;
-// }
-// </style>
+    const name = "Vue 3";
+    const arrayFrutas = [
+        {
+            name: "Manzana",
+            price: "$1.00",
+            description: "Una manzana",
+        },
+        {
+            name: "Pera",
+            price: "$2.00",
+            description: "Una pera",
+        },
+        {
+            name: "Naranja",
+            price: "$3.00",
+            description: "Una naranja",
+        },
+    ];
+</script>
+<template>
+  <h1> hola vue {{ name.toUpperCase() }}</h1>
+  <ul>
+    <li v-for="fruta in arrayFrutas" :key="fruta.name"> 
+      {{ fruta.name }} - {{ fruta.price }} - {{ fruta.description }}
+    </li>
+  </ul>
+</template>
