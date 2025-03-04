d1f9a740ec0f1c6e5b31f349e3e212c1cb607f2d
ximena <xime.mediin@gmail.com>
2025-03-04 11:12:53 -0600

    New feature :sparkles: Agregando for anidados

--- primer-vue/src/App.vue
+++ primer-vue/src/App.vue
@@ -1,28 +1,86 @@
 <script setup>
     const name = "Vue 3";
-    const arrayFrutas = [
-        {
-            name: "Manzana",
-            price: "$1.00",
-            description: "Una manzana",
-        },
-        {
-            name: "Pera",
-            price: "$2.00",
-            description: "Una pera",
-        },
-        {
-            name: "Naranja",
-            price: "$3.00",
-            description: "Una naranja",
-        },
-    ];
+const arrayFrutas = [
+    {
+        name: "Manzana",
+        price: "$1.00",
+        description: "Una manzana",
+        stock: 0,
+    },
+    {
+        name: "Pera",
+        price: "$2.00",
+        description: "Una pera",
+        stock: 10,
+    },
+    {
+        name: "Naranja",
+        price: "$3.00",
+        description: "Una naranja",
+        stock: 20,
+    },
+];
 </script>
 <template>
   <h1> hola vue {{ name.toUpperCase() }}</h1>
   <ul>
-    <li v-for="fruta in arrayFrutas" :key="fruta.name"> 
-      {{ fruta.name }} - {{ fruta.price }} - {{ fruta.description }}
-    </li>
+    <template li v-for="item in arrayFrutas" :item.name>
+      <li v-if="item.stock > 0">{{ item.name }} - {{ item.price }}</li>
+    </template>
   </ul>
 </template>
+
+<style>
+h1 {
+  color: blue;
+}
+</style>
+
+<!-------------------------OTRO EJEMPLO----------------------------------->
+
+
+<!-- <template>
+  <div>
+    <h2>Lista de usuarios:</h2>
+    <ul>
+      <template
+        v-for="(user, index) in users"
+        :key="user.id"
+      >
+        <li>
+          <h3>{{ user.name }}</h3>
+          <ul>
+            <template
+              v-for="(post, index) in user.posts"
+              :key="post.id"
+            >
+              <li>{{ post.title }}</li>
+            </template>
+          </ul>
+        </li>
+      </template>
+    </ul>
+</div>
+</template>
+
+<script setup>
+const users = [
+  {
+    id: 1,
+    name: 'Juan',
+    posts: [
+      { id: 1, title: 'Mi primer post' },
+      { id: 2, title: 'Mi segundo post' },
+    ]
+  },
+  {
+    id: 2,
+    name: 'Maria',
+    posts: [
+      { id: 3, title: 'Mi tercer post' },
+      { id: 4, title: 'Mi cuarto post' },
+    ]
+  }
+]
+</script> -->
+ 
