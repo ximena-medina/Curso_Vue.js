7b71d7e11f594577f36365b7eb51052db549f935
ximena <xime.mediin@gmail.com>
2025-03-05 10:12:22 -0600

    cambios en for anidados

--- primer-vue/src/App.vue
+++ primer-vue/src/App.vue
@@ -1,60 +1,35 @@
 <script setup>
-import {ref, computed, createCommentVNode} from 'vue'
 const name = "Vue 3";
-// computed inicia con una propiedad de flecha y simpre tiene que existir un return
 
-const counter = ref(0)
-
-const increment = () => {
-  console.log("aumentar contador") 
-  counter.value++;
-}
-const disminuir = () => {
-  console.log("disminuir contador")
-  counter.value--;
-}
-const resetear = () => {
-  console.log("resetear contador")
-  counter.value = (0)
-}
-
-const classCounter = computed(() => { 
-  if(counter.value === 0){
-    return 'cero'
-  }
-  if (counter.value > 0){
-    return 'positivo'
-  } 
-  if (counter.value < 0){
-    return 'negativo'
-  }
-})
+const arrayFrutas = [
+    {
+        name: "Manzana",
+        price: "$1.00",
+        description: "Una manzana",
+        stock: 0,
+    },
+    {
+        name: "Pera",
+        price: "$2.00",
+        description: "Una pera",
+        stock: 10,
+    },
+    {
+        name: "Naranja",
+        price: "$3.00",
+        description: "Una naranja",
+        stock: 20,
+    },
+];
 </script>
 
 <template>
-  <h1> hola {{ name.toUpperCase() }}</h1>
-  <h2 :class="classCounter"> {{ counter }}</h2>
-  <button @click="increment()">aumentar</button>
-  <button @click="disminuir()">disminuir</button>
-  <button @click="resetear()">limpiar</button>
-
+    <h1>Hola{{ name }}!</h1>
+    <ul>
+    <template v-for="fruta in arrayFrutas" :key="fruta.name">
+        <li v-if="fruta.stock > 0">
+            {{ fruta }}
+        </li>
+    </template>
+</ul>
 </template>
-
-<style>
-h1 {
-  color: blue;
-}
-
-.positivo {
-  color: green;
-}
-
-.negativo {
-  color: red;
-}
-
-.cero {
-  color: salmon;
-}
-</style>
-
