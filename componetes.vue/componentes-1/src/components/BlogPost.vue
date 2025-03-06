2158a2d47a1ad86ac18326eda3da01bf661c1269
ximena <xime.mediin@gmail.com>
2025-03-06 12:19:31 -0600

    New feature :sparkles: Incluyendo props como objeto y default value

---
+++ componetes.vue/componentes-1/src/components/BlogPost.vue
@@ -0,0 +1,24 @@
+<script setup>
+// defineProps(["title", "id", "body"])
+//objeto en defineProps
+defineProps({
+    title: String,
+    id: Number,
+    body: {
+        type: String,
+        default: "sin descripcion",
+    }
+
+})
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-body">
+        <h5 class="card-title"> {{id}} - {{ title }}</h5>
+        <p>{{ body }}</p>
+    </div>
+
+  </div>
+
+</template>
