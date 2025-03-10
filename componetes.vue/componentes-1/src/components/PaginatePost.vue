596db763340d29cd4f45386bdecf1bfc8859798e
ximena <xime.mediin@gmail.com>
2025-03-10 13:08:40 -0600

    New feature :sparkles: Realización de la paginación de la página web mediante la API jsonplaceholder

---
+++ componetes.vue/componentes-1/src/components/PaginatePost.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+const emit = defineEmits(['next', 'previus'])
+</script>
+
+<template>
+    <div class="btn-group mb-4" role="group" aria-label="Basic example">
+        <button @click="emit('Previus')" type="button" class="btn btn-primary"> Previus </button>
+        <button @click="emit('Next')" type="button" class="btn btn-primary"> Next </button>
+    </div>
+</template>
+
